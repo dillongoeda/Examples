4c158443b6e3f851f12d83591db29e7468348ba9
Dillon Goeda <dillong@korbitecinc.com>
2016-05-14 23:46:31 +0200

    Implemented the "Delete" of a Person and Unlink a friend

--- Odata/ODataDemo/Controllers/PeopleController.cs
+++ Odata/ODataDemo/Controllers/PeopleController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.OData;
 using System.Web.OData.Routing;
 using ODataDemo.Helpers;
+using ODataDemo.Models;
 
 namespace ODataDemo.Controllers
 {
@@ -32,6 +33,39 @@ namespace ODataDemo.Controllers
             return Ok(SingleResult.Create(person));
         }
 
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            var person = _context.People.Include("Friends").FirstOrDefault(p => p.PersonID == key);
+
+            if (person == null)
+                return NotFound();
+
+            var peopleWithCurrentPersonAsFriend =
+                _context.People.Include("Friends")
+                .Where(p => p.Friends.Select(f => f.PersonID).AsQueryable().Contains(key));
+
+            foreach (var p in peopleWithCurrentPersonAsFriend.ToList())
+            {
+                person.Friends.Remove(p);
+            }
+
+            _context.People.Remove(person);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        public IHttpActionResult Post([FromBody] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.People.Add(person);
+                _context.SaveChanges();
+                return Created(person);
+            }
+
+            return BadRequest("The model state of the person is invalid");
+        }
+
         [ODataRoute("People({key})/Name")]
         [ODataRoute("People({key})/Surname")]
         public IHttpActionResult GetPersonProperty([FromODataUri] int key)
@@ -54,6 +88,29 @@ namespace ODataDemo.Controllers
             return ODataHelpers.CreateOKHttpActionResult(this, value);
         }
 
+        [ODataRoute("People({key})/Name/$value")]
+        [ODataRoute("People({key})/Surname/$value")]
+        public IHttpActionResult GetPersonRawProperty([FromODataUri] int key)
+        {
+            var person = _context.People.FirstOrDefault(p => p.PersonID == key);
+
+            if (person == null)
+                return NotFound();
+
+            var property = Url.Request.RequestUri
+                 .Segments[Url.Request.RequestUri.Segments.Length - 2].TrimEnd('/');
+
+            if (!person.HasProperty(property))
+                return NotFound();
+
+            var value = person.GetValue(property);
+
+            if (string.IsNullOrWhiteSpace(value.ToString()))
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+
+            return ODataHelpers.CreateOKHttpActionResult(this, value.ToString());
+
+        }
 
         [ODataRoute("People({key})/Devices")]
         [EnableQuery]
@@ -103,7 +160,7 @@ namespace ODataDemo.Controllers
 
             return Ok(person.Friends);
         }
-
+        
 
         [HttpPost]
         [ODataRoute("People({key})/Friends/$ref")]
@@ -131,20 +188,27 @@ namespace ODataDemo.Controllers
             return Ok();
         }
 
-        public IHttpActionResult Delete([FromODataUri] int key)
+        [HttpDelete]
+        [ODataRoute("People({key})/Friends({relatedKey})/$ref")]
+        public IHttpActionResult DeleteFriend([FromODataUri] int key, [FromODataUri] int relatedKey)
         {
-            var person = _context.People.FirstOrDefault(p => p.PersonID == key);
+            var person = _context.People.Include("Friends").FirstOrDefault(p => p.PersonID == key);
 
             if (person == null)
                 return NotFound();
 
-            _context.People.Remove(person);
-            _context.SaveChanges();
+            var friend = _context.People.FirstOrDefault(p => p.PersonID == relatedKey);
+
+            if (friend == null)
+                return NotFound();
+
+            person.Friends.Remove(friend);
+
             return Ok();
         }
 
 
-        protected override void Dispose(bool disposing)
+       protected override void Dispose(bool disposing)
         {
             _context.Dispose();
             base.Dispose(disposing);
